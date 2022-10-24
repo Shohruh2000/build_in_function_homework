7f23a180cffc202ca32035254a58c8747d66fe25
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-24 21:54:10 +0500

    build_func05.py

--- build_func05.py
+++ build_func05.py
@@ -1,2 +1,9 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func05
+from math import pow 
+def main(n,x):
+    s = 1
+    s *=pow(x,n)+pow(n,x)
+    return int(s)
+answer = main(3,6)
+print(answer)
