4606ff7de5484e1ac21124194d8d23496181692f
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-24 21:41:24 +0500

    build_func04.py

--- build_func04.py
+++ build_func04.py
@@ -1,2 +1,7 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func04
+from math import pow
+def main(n):
+    return pow(((2+n)/3),2)
+answer = main(4)
+print(answer)
