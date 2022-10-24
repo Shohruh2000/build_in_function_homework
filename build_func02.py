2162c80d772ae43597c4705cfdb0bd2ca606bc7c
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-24 21:29:22 +0500

    build_func

--- build_func02.py
+++ build_func02.py
@@ -1,3 +1,9 @@
 # the task condition is placed in the README file
 #ttps://github.com/codeschool43/Build_in_function_homework#build_func02
-print(round(3*(7/5-9/4),2))
+
+def main():
+    x = round(3*(7/5-9/4),2)
+    print(x)
+main()
+
+
