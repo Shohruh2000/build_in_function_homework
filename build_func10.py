2f615e840bd99901d70e0e89477eb76467c7dfc0
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-24 22:29:57 +0500

    build_func10.py

--- build_func10.py
+++ build_func10.py
@@ -1,2 +1,10 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func10
+from math import pow
+def main(x,y):
+    s = 1
+    s *=( 3*pow(y,(1/2))+pow(x,(2/3)))
+    return s
+
+answer = main(8,4)
+print(answer)
