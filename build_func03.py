069c5e26be4ea5dd35c1d59f8fe99134099046f8
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-24 21:35:57 +0500

    build_func03.py

--- build_func03.py
+++ build_func03.py
@@ -2,6 +2,8 @@
 # https://github.com/codeschool43/Build_in_function_homework#build_func03
 from math import pow
 def main(n):
-    return  3*(pow((n+1),2))
+    
+    a = 3*(pow((n+1),2))
+    return a
 answer = main(3.5)
 print(answer)
