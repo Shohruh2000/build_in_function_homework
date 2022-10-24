886aaf83dea7f51363406e6d869aef9e0a84392c
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-24 21:45:39 +0500

    build_func

--- build_func04.py
+++ build_func04.py
@@ -2,6 +2,8 @@
 # https://github.com/codeschool43/Build_in_function_homework#build_func04
 from math import pow
 def main(n):
-    return pow(((2+n)/3),2)
+    s = 1
+    s *= pow(((2+n)/3),2)
+    return s
 answer = main(4)
 print(answer)
