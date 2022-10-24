6fc03b63b02a6ebeaa35f3440af489ceddd09860
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-24 21:32:56 +0500

    build_funy

--- build_func03.py
+++ build_func03.py
@@ -1,6 +1,7 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func03
 from math import pow
-n = 3.5
-answer = 3*(pow((n+1),2))
+def main(n):
+    return  3*(pow((n+1),2))
+answer = main(3.5)
 print(answer)
