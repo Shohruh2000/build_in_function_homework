f57f3b8db6d0b01c4c053b8e8137876db87d34e9
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-24 22:01:47 +0500

    build_func06.py

--- build_func06.py
+++ build_func06.py
@@ -1,2 +1,7 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func06
+
+def main(a):
+    return round(a,2)
+answer = main(3.456)
+print(answer)
