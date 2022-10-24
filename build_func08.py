9fc979314eaf23b4f723e45d3609f7aa322b5248
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-24 22:15:44 +0500

    buld_func08.py

--- build_func08.py
+++ build_func08.py
@@ -1,2 +1,10 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func08
+from math import pow 
+def main(x,y):
+    s = 1
+    s *= (5*(pow(x,2)*pow(y,3))+x*pow(y,2))
+    return int(s)
+
+answer = main(7,1)
+print(answer)
